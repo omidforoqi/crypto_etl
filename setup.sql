caf072d891d091357b12283df20571419dec9c4d
Omid Foroughi <foroqi@proton.me>
2024-06-30 00:58:57 +0330

    add dump of metabase to docker compose

--- setup.sql
+++ setup.sql
@@ -1,24 +1,24 @@
 CREATE DATABASE IF NOT EXISTS crypto_db;
 
-USE crypto_db;
+-- USE crypto_db;
 
-CREATE TABLE IF NOT EXISTS bitstamp
-(
-    coin_name String,
-    open Float32,
-    close Float32,
-    high Float32,
-    low Float32,
-    timestamp DateTime
-) ENGINE = MergeTree()
-ORDER BY (coin_name, timestamp);
+-- CREATE TABLE IF NOT EXISTS bitstamp
+-- (
+--     coin_name String,
+--     open Float32,
+--     close Float32,
+--     high Float32,
+--     low Float32,
+--     timestamp DateTime
+-- ) ENGINE = MergeTree()
+-- ORDER BY (coin_name, timestamp);
 
-CREATE TABLE IF NOT EXISTS transactions
-(
-    coin_name String,
-    amount Float32,
-    timestamp DateTime,
-    price Float32,
-    type Bool
-) ENGINE = MergeTree()
-ORDER BY (coin_name, timestamp, amount);
+-- CREATE TABLE IF NOT EXISTS transactions
+-- (
+--     coin_name String,
+--     amount Float32,
+--     timestamp DateTime,
+--     price Float32,
+--     type Bool
+-- ) ENGINE = MergeTree()
+-- ORDER BY (coin_name, timestamp, amount);
